60e28ede35641f73029484d07286a1337edf27e8
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2023-01-17 09:31:12 -0300

    Criação da camada model

---
+++ src/main/java/com/login/jwt/auth/model/Usuario.java
@@ -0,0 +1,39 @@
+package com.login.jwt.auth.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "tb_usuario")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String sobrenome;
+
+    @Column(nullable = false)
+    private String email;
+
+}
